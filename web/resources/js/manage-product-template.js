3b5a992c88359261befd720af23aa8fefa79381b
nazmulhasan <nazmul@apurbatech.com>
2017-04-25 17:59:30 +0600

    manage product template is added

---
+++ web/resources/js/manage-product-template.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var manage_product_template_module_1 = require("./app/templates/manage-product-template-module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(manage_product_template_module_1.ManageProductAppModule);
+//# sourceMappingURL=manage-product-template.js.map
---
+++ web/resources/js/manage-product-template.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { ManageProductAppModule } from './app/templates/manage-product-template-module';
+
+platformBrowserDynamic().bootstrapModule(ManageProductAppModule);
