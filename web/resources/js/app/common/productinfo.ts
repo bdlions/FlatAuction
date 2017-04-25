ff6a1e741bd06c4cec871f67cde4eaf099c1f8b8
nazmulhasan <nazmul@apurbatech.com>
2017-04-25 15:48:59 +0600

    duration filer, bids link up from product

--- web/resources/js/app/common/productinfo.ts
+++ web/resources/js/app/common/productinfo.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Http} from '@angular/http';
 import {Subscription} from 'rxjs';
 import {Product} from '../dto/Product';
@@ -14,7 +14,7 @@ export class Productinfo implements OnInit, OnDestroy {
     private subscribe:Subscription;
     private id:number;
     
-    constructor(public router: ActivatedRoute) {
+    constructor(public router:Router, public route: ActivatedRoute) {
         this.product = new Product();
         this.product.id = 1;
         this.product.title = "Fun at the Bowling Alley";
@@ -25,10 +25,17 @@ export class Productinfo implements OnInit, OnDestroy {
         this.product.type = 1;
         this.product.location_type = "location1";
         this.product.images = JSON.parse("[{\"id\":\"1\", \"url\":\"a.jpg\"}, {\"id\":\"2\", \"url\":\"b.jpg\"}]");
+        this.product.totalBidders = 10;
+        this.product.totalBids = 36;
+        this.product.timeLeft = "1 day 13 hours 30 mins";
+    }
+    
+    public showBids(event: Event, id: number){
+        this.router.navigate(['bids', {id: this.id }]);
     }
     
     ngOnInit() {
-        this.subscribe = this.router.params.subscribe(params => {
+        this.subscribe = this.route.params.subscribe(params => {
             this.id = params['id']; 
             console.log(this.id);
         });
