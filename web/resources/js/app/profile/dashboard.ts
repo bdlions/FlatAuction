4044444d0427a9cb4da931bf2e876656774056c1
nazmulhasan <nazmul@apurbatech.com>
2017-04-30 05:40:44 +0600

    Profile info at dashboard

--- web/resources/js/app/profile/dashboard.ts
+++ web/resources/js/app/profile/dashboard.ts
@@ -8,9 +8,15 @@ import {User} from '../dto/User'
     templateUrl: window.SUB_DIRECTORY +"/html_components/member/profile/index.html",
 })
 export class DashBoard {
-    
+    private user: User;
     constructor(public router: Router, public http: Http) {
-        
+        this.user = new User();
+        this.user.userId = "u1"; 
+        this.user.firstName = "Nazmul";
+        this.user.lastName = "Hasan";
+        this.user.email = "bdlions@gmail.com";
+        this.user.cellNo = "8801678112509";
+        this.user.img = "a.jpg";
     }
     
 }
