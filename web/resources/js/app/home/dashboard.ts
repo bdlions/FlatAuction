76371ac9526f1558f7d9ce08d32aae6b90a8b9fb
nazmulhasan <nazmul@apurbatech.com>
2017-04-30 13:28:32 +0600

    Location List is added for home

--- web/resources/js/app/home/dashboard.ts
+++ web/resources/js/app/home/dashboard.ts
@@ -2,15 +2,23 @@ import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {Http} from '@angular/http';
 import {User} from '../dto/User'
+import {Location} from '../dto/Location';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
     selector: 'data-content1ddd',
     templateUrl: window.SUB_DIRECTORY +"/html_components/member/home/index.html",
 })
 export class DashBoard {
-    
+    public selectedLocation:string;
+    private locationList: Location[];
     constructor(public router: Router, public http: Http) {
-        
+        this.locationList = JSON.parse("[{\"id\":\"1\", \"locationType\":\"area\",\"searchString\":\"London\"}, {\"id\":\"2\", \"locationType\":\"area\",\"searchString\":\"London 123\"}, {\"id\":\"3\", \"locationType\":\"area\",\"searchString\":\"London 456\"}]");
     }
     
+    search(event: Event, id: number) {
+        event.preventDefault();
+        this.router.navigate(['search', {id: id}]);
+    }
 }
