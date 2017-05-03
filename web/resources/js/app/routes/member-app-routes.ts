c090597422be9004ca3fb2063b0f562097d68809
nazmulhasan <nazmul@apurbatech.com>
2017-05-03 08:51:03 +0600

    AuthGuard is removed from member template temporarily

--- web/resources/js/app/routes/member-app-routes.ts
+++ web/resources/js/app/routes/member-app-routes.ts
@@ -9,14 +9,14 @@ import {Messages} from "../member/messages";
 
 // Define which component should be loaded based on the current URL
 export const routes: Routes = [
-    {path: '', component: DashBoard, canActivate: [AuthGuard]},
-    {path: 'dashboard', component: DashBoard, canActivate: [AuthGuard]},
+    {path: '', component: DashBoard},
+    {path: 'dashboard', component: DashBoard},
     //    {path: 'signup', component: Signup},
     //  { path: 'signup', component: Signup },
-    {path: 'savedads', component: SavedAds, canActivate: [AuthGuard]},
-    {path: 'myads', component: MyAds, canActivate: [AuthGuard]},
-    {path: 'messages', component: Messages, canActivate: [AuthGuard]},
+    {path: 'savedads', component: SavedAds},
+    {path: 'myads', component: MyAds},
+    {path: 'messages', component: Messages},
     //{path: 'home', component: DashBoard, canActivate: [AuthGuard]},
     //  { path: 'abroad',   component: Abroad, canActivate: [AuthGuard] },
-    {path: '**', component: DashBoard, canActivate: [AuthGuard] },
+    {path: '**', component: DashBoard},
 ];
